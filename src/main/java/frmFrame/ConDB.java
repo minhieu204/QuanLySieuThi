5055bc9b94922bcd534460971bd29e33aaf1645b
minhieu204 <minhhieu13062004@gmail.com>
2024-09-28 11:08:25 +0700

    commit qlbh

---
+++ src/main/java/frmFrame/ConDB.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frmFrame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Admin
+ */
+public class ConDB {
+    public static Connection ketnoiDB(){
+        Connection con=null;
+        try {
+            //B1: Nạp trình điều khiển
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=qlsach;encrypt=true;trustServerCertificate=true";
+            String user = "sa";
+            String password = "1306";
+            con=DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
