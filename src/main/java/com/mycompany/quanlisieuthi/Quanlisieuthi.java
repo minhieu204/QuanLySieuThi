34c2df8abe6ce9bde00a35a74a5f164f1ca409bf
minhieu204 <minhhieu13062004@gmail.com>
2024-09-28 09:56:00 +0700

    first commit

---
+++ src/main/java/com/mycompany/quanlisieuthi/Quanlisieuthi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlisieuthi;
+
+/**
+ *
+ * @author Admin
+ */
+public class Quanlisieuthi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
